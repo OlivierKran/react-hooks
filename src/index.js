71fa31f6a953051ee35d00ca154a65f102c1f91f
Oliv <82574961+OlivierKran@users.noreply.github.com>
2024-09-10 11:11:10 +0200

    :art: création d'un hook personnalisé

--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ import './index.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+//Hook personnalisé
+
+function useIncrement(init, step) {
+  const [count3, setCount3] = useState(init);
+  const increment = () => {
+    setCount3(c => c + step)
+  }
+  return [count3, increment]
+}
+
 function Compteur() {
   const [count, setCount] = useState(0);
   const [count2, setCount2] = useState(0);
@@ -20,9 +30,14 @@ function Compteur() {
     e.preventDefault();
     setCount2(c => c + 2)
   }
+
+  const [count3, increment] = useIncrement(0, 5);
+
   return <>
-  <button onClick={handleClick}>Incrémenter {count}</button>
-  <button onClick={handleClick2}>Incrémenter {count2}</button>
+    <button onClick={handleClick}>Incrémenter {count}</button>
+    <button onClick={handleClick2}>Incrémenter {count2}</button>
+    <br></br>
+    <button onClick={increment}>Incrémenter {count3} </button>
   </>
 }
 
